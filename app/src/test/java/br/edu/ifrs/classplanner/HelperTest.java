6e0f8efdb2b6ea14e03c8dfafbd41b0bb8785f50
gui.parzianello <glhrm_@live.com>
2021-04-02 16:59:27 -0300

    first commit for tests

---
+++ app/src/test/java/br/edu/ifrs/classplanner/HelperTest.java
@@ -0,0 +1,28 @@
+package br.edu.ifrs.classplanner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.edu.ifrs.classplanner.helper.Helper;
+
+public class HelperTest {
+
+    @Test
+    public void dateMustBeValid() {
+        boolean result = Helper.isValidDateOrTime("01/05/2021", Helper.DATE);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void dateMustNotBeValid() {
+        boolean result = Helper.isValidDateOrTime("31/04/2021", Helper.DATE);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void mustNotBeAHoliday_wrongFormat() {
+        boolean result = Helper.isHoliday("2021/05/01");
+        Assert.assertFalse(result);
+    }
+
+}
