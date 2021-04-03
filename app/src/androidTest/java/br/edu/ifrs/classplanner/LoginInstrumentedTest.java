af30051ebc3540e2756bdb6425c253b6948d8504
gui.parzianello <glhrm_@live.com>
2021-04-03 19:22:54 -0300

    commenting tests

--- app/src/androidTest/java/br/edu/ifrs/classplanner/LoginInstrumentedTest.java
+++ app/src/androidTest/java/br/edu/ifrs/classplanner/LoginInstrumentedTest.java
@@ -19,12 +19,19 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+/*
+Testa os métodos do processo de login
+ */
 @RunWith(AndroidJUnit4.class)
 public class LoginInstrumentedTest {
 
     @Rule
     public ActivityScenarioRule<AuthActivity> activityRule = new ActivityScenarioRule<>(AuthActivity.class);
 
+    /*
+    Para que o teste ocorra, o usuário não deve estar logado e a tela inicial deve ser AuthActivity.
+    Em seguida, o usuário deve ser direcionado ao LoginFragment por meio de clique no botão de login.
+     */
     @Before
     public void checkUserIsNotLoggedInAndGoToLoginView() {
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
@@ -34,6 +41,10 @@ public class LoginInstrumentedTest {
         onView(withId(R.id.buttonOptionLogin)).perform(click());
     }
 
+    /*
+    Testa se a snackbar com texto de alerta é exibida
+    quando o usuário tentar fazer login com o campo email vazio.
+     */
     @Test
     public void snackbarIsShown_EmailIsEmpty() {
         String stringToBeTyped = "password";
@@ -45,6 +56,10 @@ public class LoginInstrumentedTest {
                 .check(matches(withText("Preencha todos os campos")));
     }
 
+    /*
+    Testa se a snackbar com texto de alerta é exibida
+    quando o usuário tentar fazer com o campo senha vazio.
+     */
     @Test
     public void snackbarIsShown_PasswordIsEmpty() {
         String stringToBeTyped = "email";
--- app/src/test/java/br/edu/ifrs/classplanner/HelperUnitTest.java
+++ app/src/test/java/br/edu/ifrs/classplanner/HelperUnitTest.java
@@ -5,20 +5,33 @@ import org.junit.Test;
 
 import br.edu.ifrs.classplanner.helper.Helper;
 
+/*
+Testa os métodos da classe Helper
+ */
 public class HelperUnitTest {
 
+    /*
+    Data é válida e está no formato correto. Logo, o teste deve retornar verdadeiro.
+     */
     @Test
     public void dateMustBeValid() {
         boolean result = Helper.isValidDateOrTime("01/05/2021", Helper.DATE);
         Assert.assertTrue(result);
     }
 
+    /*
+    Apesar de estar no formato correto, a data é inválida. Logo, o teste deve retornar falso.
+     */
     @Test
     public void dateMustNotBeValid() {
         boolean result = Helper.isValidDateOrTime("31/04/2021", Helper.DATE);
         Assert.assertFalse(result);
     }
 
+    /*
+    Apesar de a data ser um feriado, está no formato incorreto (deveria ser DD/MM/YYYY).
+    Logo, o teste deve retornar falso.
+     */
     @Test
     public void mustNotBeAHoliday_wrongFormat() {
         boolean result = Helper.isHoliday("2021/05/01");
