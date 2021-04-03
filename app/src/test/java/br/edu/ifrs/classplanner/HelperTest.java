bf964cf34d71d96097c8127486838c73ae8f09cf
gui.parzianello <glhrm_@live.com>
2021-04-02 22:27:49 -0300

    fixing failed test

--- app/src/test/java/br/edu/ifrs/classplanner/HelperTest.java
+++ app/src/test/java/br/edu/ifrs/classplanner/HelperTest.java
@@ -22,7 +22,7 @@ public class HelperTest {
     @Test
     public void mustNotBeAHoliday_wrongFormat() {
         boolean result = Helper.isHoliday("2021/05/01");
-        Assert.assertTrue(result);
+        Assert.assertFalse(result);
     }
 
 }
